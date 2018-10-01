3856399521450a8bb6be8724d944987b696fb877
Chris <chrislee8@gmail.com>
2018-09-30 23:29:33 -0400

    topic3 challenge

---
+++ topic3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace topic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pie = 3.1415926;
+            double radius = 5;
+            double height = 2;
+            double volumn = pie * radius * radius * height;
+            Console.WriteLine($"The Volumn is { volumn }");
+        }
+    }
+}
