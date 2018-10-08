b1ef38db4e3916d1091941339d4ff67b975eada8
Chris <chrislee8@gmail.com>
2018-10-08 19:22:27 -0400

    insert sort

---
+++ insertsort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace insertsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] original = {0, 3, 9, 2, 1, 7, 5};
+            for (int i=0;i<original.Length;i++)
+            {
+                Console.Write(original[i] + ", ");
+            }
+            Console.WriteLine("");
+            int[] after = insertsort(original);
+            for (int i=0;i<after.Length;i++)
+            {
+                Console.Write(after[i] + ", ");
+            }
+        }
+
+        static int[] insertsort(int[] before)
+        {
+            for (int i=1;i<before.Length;i++)
+            {
+                for (int j=i;j>0;j--)
+                {
+                    if (before[j] < before[j-1])
+                    {
+                        int temp = before[j];
+                        before[j] = before[j-1];
+                        before[j-1] = temp;
+                    }
+                }
+
+            }
+            return before;
+        }
+    }
+}
