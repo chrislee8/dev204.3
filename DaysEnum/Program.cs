5e3dee230e47a8264bf49f5006152d62ba7313a3
Chris Lee <chris.lee@PIXMAC-L1PVHTDF.fios-router.home>
2018-10-23 15:12:46 -0400

    Days Enum

---
+++ DaysEnum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DaysEnum
+{
+    class Program
+    {
+        enum Days {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thur,
+            Fri,
+            Sat
+        }
+        static void Main(string[] args)
+        {
+            int x = (int)Days.Sun;
+            int y = (int)Days.Fri;
+
+            Console.WriteLine("Sun = {0}", x);
+            Console.WriteLine("Fri = {0}", y);
+            Console.WriteLine(Days.Thur);
+        }
+    }
+}
