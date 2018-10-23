ec64434dc07f55cc6e49d8f13d54ee441cf3d907
Chris Lee <chris.lee@PIXMAC-L1PVHTDF.fios-router.home>
2018-10-22 21:38:43 -0400

    Car Struct Done

--- CarStruct/Program.cs
+++ CarStruct/Program.cs
@@ -2,11 +2,26 @@
 
 namespace CarStruct
 {
+    struct Car {
+        public string Make;
+        public string Model;
+        public string Color;
+
+        public void Accelerate() {
+            Console.WriteLine("Vroom!");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car MyCar = new Car();
+            MyCar.Make = "Mercedes";
+            MyCar.Model = "GL450";
+            MyCar.Color = "Black";
+
+            Console.WriteLine("My car is a {0} {1} that is {2} in color.", MyCar.Make, MyCar.Model, MyCar.Color);
+            MyCar.Accelerate();
         }
     }
 }
